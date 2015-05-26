59b102d20ad73c2213dfb3ef46791580eda0772e
Max Johansen <macsj200@gmail.com>
2015-05-25 18:14:42 -0600

    increment version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'maxjohansen:simplestripe',
-  version: '0.0.1',
+  version: '0.0.2',
   summary: 'A simplified version of https://github.com/tyler-johnson/stripe-meteor',
   git: 'https://github.com/macsj200/simplestripe',
   documentation: 'README.md'
