5b1d6d69c870da6f3f1d9c4c80442eaf8fefeffa
Max Johansen <maxjohansen@berkeley.edu>
2015-06-15 19:42:10 -0600

    update version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'maxjohansen:simplestripe',
-  version: '0.1.9',
+  version: '0.1.9.1',
   summary: 'A simplified version of https://github.com/tyler-johnson/stripe-meteor',
   git: 'https://github.com/macsj200/simplestripe',
   documentation: 'README.md'
